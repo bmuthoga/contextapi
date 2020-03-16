12b3c3ef483e8367ecc789c3d84f562ee0ad8014
Batian <bmuthoga@gmail.com>
2020-03-17 02:08:55 +0300

    feat(multiple-contexts-using-hooks): consume multiple contexts using hooks
    - refactor Navbar.js to functional component
    - refactor ThemeToggle.js to functional component
    - consume ThemeContext and AuthContext in Navbar.js using useContext hook
    - consume ThemeContext in ThemeToggle.js using useContext hook

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,33 +1,25 @@
-import React, { Component } from 'react'
+import React, { useContext } from 'react'
 import { ThemeContext } from '../contexts/ThemeContext'
 import { AuthContext } from '../contexts/AuthContext'
 
-class Navbar extends Component {
-  render() {
-    return (
-      <AuthContext.Consumer>{authContext => (
-        <ThemeContext.Consumer>{themeContext => {
-            const { isAuthenticated, toggleAuth } = authContext
-            const { isLightTheme, light, dark } = themeContext
-            const theme = isLightTheme ? light : dark
+const Navbar = () => {
+  const { isAuthenticated, toggleAuth } = useContext(AuthContext)
+  const { isLightTheme, light, dark } = useContext(ThemeContext)
+  const theme = isLightTheme ? light : dark
 
-            return (
-              <nav style={{ background: theme.ui, color: theme.syntax }}>
-                <h1>Context App</h1>
-                <div onClick={toggleAuth} style={{ userSelect: "none", cursor: "pointer" }}>
-                  {isAuthenticated ? 'Logged In' : 'Logged Out'}
-                </div>
-                <ul style={{ userSelect: "none" }}>
-                  <li style={{ cursor: "pointer" }}>Home</li>
-                  <li style={{ cursor: "pointer" }}>About</li>
-                  <li style={{ cursor: "pointer" }}>Contact</li>
-                </ul>
-              </nav>
-            )
-          }}</ThemeContext.Consumer>
-      )}</AuthContext.Consumer>
-    )
-  }
+  return (
+    <nav style={{ background: theme.ui, color: theme.syntax }}>
+      <h1>Context App</h1>
+      <div onClick={toggleAuth} style={{ userSelect: "none", cursor: "pointer" }}>
+        {isAuthenticated ? 'Logged In' : 'Logged Out'}
+      </div>
+      <ul style={{ userSelect: "none" }}>
+        <li style={{ cursor: "pointer" }}>Home</li>
+        <li style={{ cursor: "pointer" }}>About</li>
+        <li style={{ cursor: "pointer" }}>Contact</li>
+      </ul>
+    </nav>
+  )
 }
 
 export default Navbar
